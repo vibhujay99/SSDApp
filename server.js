e8f866c87fd0cc3cd09c655e4bb41b2d236d06d5
Vibhujay99 <vibhujay99@gmail.com>
2023-09-30 01:37:23 +0530

    Ref() : Helmet js

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 import express from "express";
-import colors from "colors";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import connectDB from "./config/db.js";
@@ -7,35 +6,39 @@ import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
+import helmet from "helmet"; // Import Helmet middleware
 
-//configure env
+// Configure environment variables
 dotenv.config();
 
-//databse config
+// Database configuration
 connectDB();
 
-//rest object
+// Create the Express application
 const app = express();
 
-//middelwares
+// Middleware
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
-//routes
+// Security headers using Helmet middleware
+app.use(helmet());
+
+// Routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
-//rest api
+// REST API
 app.get("/", (req, res) => {
   res.send("<h1>Welcome to ecommerce app</h1>");
 });
 
-//PORT
+// PORT
 const PORT = process.env.PORT || 5000;
 
-//run listen
+// Run the server
 app.listen(PORT, () => {
   console.log(
     `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan
