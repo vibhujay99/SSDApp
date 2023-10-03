f61711959139247d38227f2e7115b099f2582c95
oshanu <oshan.udumullaarachchi@directfn.com>
2023-10-03 08:09:35 +0530

    fixing the security vulnerability of Lack of proper error handling and Logging

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -4,14 +4,37 @@ import userModel from "../models/userModel.js";
 //Protected Routes token base
 export const requireSignIn = async (req, res, next) => {
   try {
-    const decode = JWT.verify(
-      req.headers.authorization,
-      process.env.JWT_SECRET
-    );
-    req.user = decode;
+   // Get the JWT token from the 'authorization' header
+    const token = req.headers.authorization;
+
+    // If the 'authorization' header is missing, return a 401 Unauthorized response
+    if (!token) {
+      return res.status(401).json({ message: 'Authorization header is missing' });
+  }
+    // Verify the JWT token using the provided JWT_SECRET
+    const decoded = JWT.verify(token, process.env.JWT_SECRET);
+
+    // Attach the decoded user information to the request object
+    req.user = decoded;
+
+    // Continue to the next middleware or route handler
     next();
-  } catch (error) {
-    console.log(error);
+  }catch (error) {
+    // Log the error for debugging purposes
+    console.error(error);
+
+    // If the error is a JsonWebTokenError, return a 401 Unauthorized response
+    if (error.name === 'JsonWebTokenError') {
+      return res.status(401).json({ message: 'Invalid token' });
+    }
+
+    // If the error is a TokenExpiredError, return a 401 Unauthorized response
+    if (error.name === 'TokenExpiredError') {
+      return res.status(401).json({ message: 'Token has expired' });
+    }
+
+    // Handle other unexpected errors with a 500 Internal Server Error response
+    return res.status(500).json({ message: 'Internal server error' });
   }
 };
 
